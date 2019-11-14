daa2f84adc15be5e51b96742524e9f7a5b4ef850
Gavan Lamb <gavan.jon.lamb@gmail.com>
2019-11-14 21:38:59 +1100

    remove backend section and update profile name

--- main.tf
+++ main.tf
@@ -1,15 +1,7 @@
 provider "aws" {
   region = "ap-southeast-2"
   shared_credentials_file = "/Users/gavanlamb/.aws/Credentials"
-  profile = "default"
-}
-
-terraform {
-  backend "s3" {
-    bucket = "luga-terraform-remote-state"
-    key = "key"
-    region = "ap-southeast-2"
-  }
+  profile = "luga"
 }
 
 module "backend" {
