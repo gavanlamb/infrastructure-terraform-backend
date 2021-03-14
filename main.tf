ea9131a99768ac38ae1d72147014ff83a14c3165
Gavan Lamb <gavan.jon.lamb@gmail.com>
2021-03-14 16:49:12 +1100

    Map variables to module properties

--- main.tf
+++ main.tf
@@ -1,17 +1,15 @@
 provider "aws" {
   region = "ap-southeast-2"
-  shared_credentials_file = "/Users/gavanlamb/.aws/Credentials"
-  profile = "luga"
+  version = "3.32.0"
 }
 
 module "backend" {
-  source = "github.com/Haplo-tech/Terraform.Module.AWS.Backend"
-  company = "luga"
-  environment = "infrastructure"
-  name = "terraform-backend"
-  service = "terraform"
-  group_name = "terraform-remote-state"
-  iam_path = "terraform"
-  bucket_name = "4df9-terraform-remote-state"
-  dynamodb_name = "terraform-remote-state-lock"
+  source = "github.com/gavanlamb/terraform-module-aws-backend"
+  environment    = var.environment
+  name           = var.name
+  iam_path	     = var.iam_path
+  username       = var.username
+  user_pgp_key   = var.user_pgp_key
+  bucket_name    = var.bucket_name
+  dynamodb_name  = var.dynamodb_name
 }
